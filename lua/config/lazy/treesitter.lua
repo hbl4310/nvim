5bf920399925899368358549cd1699532a7ccdad
hanbo <hanbo4310@gmail.com>
2026-03-29 22:37:33 +0100

    better treesitter config

--- lua/config/lazy/treesitter.lua
+++ lua/config/lazy/treesitter.lua
@@ -1,16 +1,23 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter")
-    configs.setup({
-      ensure_installed = {
-        "help", "c", "rust", "lua", "vim", "vimdoc", "javascript", "typescript", "html", "python", "julia"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter")
+        configs.setup({
+            -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+            install_dir = vim.fn.stdpath('data') .. '/site',
+            ensure_installed = {
+               "help", "c", "rust", "lua", "vim", "vimdoc", "javascript", "typescript", "html", "python", "julia"
+            },
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+        configs.install({ "python", "julia", "rust", "javascript", "typescript" })
+    end
 }
